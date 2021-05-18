b7f56ef6041347b62b90a74e7f18266a73576b6a
moha6248 <51365122+moha6248@users.noreply.github.com>
2021-05-18 13:33:22 +0200

    login

---
+++ examProject21/Controllers/loginController.cs
@@ -0,0 +1,37 @@
+﻿using examProject21.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace examProject21.Controllers
+{
+    public class loginController : Controller
+    {
+        public String UserName = "admin";
+        public String Password = "admin";
+
+        [HttpGet]
+        public ActionResult loginForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult loginForm(Login login)
+        {
+            if(login.UserName == UserName && login.Password == Password)
+            {
+            return View(new Uri("https://www.youtube.com/"));
+
+            }
+            else{
+            return View();
+        }
+       }   
+    
+}
+}
+
+
+
