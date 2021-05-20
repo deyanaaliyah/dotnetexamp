71c5cb1be04032e49b595dfb2758bf08286286b1
mugisha98 <mugishah@hotmail.com>
2021-05-20 12:07:37 +0200

    test

--- examProject21/Controllers/EmpController1.cs
+++ examProject21/Controllers/EmpController1.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using examProject21.Models;
 using Microsoft.EntityFrameworkCore;
 
